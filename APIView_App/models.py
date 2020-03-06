ed091f17374750e7c877edc1b1d10e6b3b3eb8be
srinivasrao452 <30620328+srinivasrao452@users.noreply.github.com>
2020-03-06 09:07:36 +0530

    Update models.py

--- APIView_App/models.py
+++ APIView_App/models.py
@@ -2,6 +2,8 @@
 from django.db import models
 # This is comment
 
+# From server to local
+
 class Employee(models.Model):
     eno = models.IntegerField(primary_key=True)
     ename= models.CharField(max_length=30)
